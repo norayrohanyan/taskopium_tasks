461388fa4a3ecd5cbe602f5b566651505d702625
norayrohanyan <norayrohanyan27@gmail.com>
2024-02-20 17:19:12 +0400

    first commit

---
+++ task14/script.js
@@ -0,0 +1,24 @@
+const form = document.querySelector('form');
+
+document.addEventListener('DOMContentLoaded', () => {
+    getLocalStorage();
+
+    form.addEventListener('input', () => {
+        updateLocalStorage();
+    })
+})
+
+function updateLocalStorage() {
+    const fields = form.querySelectorAll('input');
+    fields.forEach(field => {
+        localStorage.setItem(field.id, field.value);
+        console.log(field.value);
+    })
+}
+
+function getLocalStorage() {
+    const fields = form.querySelectorAll('input');
+    fields.forEach(field => {
+        field.value = localStorage.getItem(field.id);
+    })
+}
